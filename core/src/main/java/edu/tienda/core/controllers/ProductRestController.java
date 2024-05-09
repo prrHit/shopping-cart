28a498c846335f84b422e373e744568b814f51a9
pedro <prr@mail.com>
2024-05-09 11:06:35 -0300

    agregamos codigo para consumir datos de api externa

--- core/src/main/java/edu/tienda/core/controllers/ProductRestController.java
+++ core/src/main/java/edu/tienda/core/controllers/ProductRestController.java
@@ -6,13 +6,16 @@ import edu.tienda.core.domain.Product;
 import edu.tienda.core.services.ProductsService;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -33,4 +36,17 @@ public class ProductRestController {
         // Put the products obtained from the service into responses' body
         return ResponseEntity.ok(products);
     }
+
+    @GetMapping("/fake-products")
+    public ResponseEntity<?> fakeProductsAPI() {
+        List<Product> products = new ArrayList<>(Arrays.asList(
+            new Product(1, "Camiseta Juventus", 1200.0,4),
+            new Product(2, "Camiseta Boca Juniors", 1000.0, 10),
+            new Product(3, "Camiseta de San Martin", 900.0, 8)
+        ));
+
+        // return products in response's body
+        return ResponseEntity.ok(products);
+    }
+    
 }
---
+++ core/src/main/java/edu/tienda/core/services/ProductsServiceImplExternAPI.java
@@ -0,0 +1,33 @@
+package edu.tienda.core.services;
+
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import edu.tienda.core.domain.Product;
+
+@Service("API")
+@ConditionalOnProperty(
+    value="products.strategy",
+    havingValue = "IN_ANOTHER_API"
+)
+public class ProductsServiceImplExternAPI implements ProductsService{
+    @Override
+    public List<Product> getProducts() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<List<Product>> response = restTemplate.
+            exchange("http://localhost:8081/tienda/api/v1/products/fake-products", 
+            HttpMethod.GET, 
+            null, 
+            new ParameterizedTypeReference<List<Product>>() {
+            });
+         
+        List<Product> products = response.getBody();    
+        
+        return products;
+}
